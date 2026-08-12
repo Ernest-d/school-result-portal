d57d065dae6fdebaf9f5538bca17a9586097c283
Ernest-d <ernest.boy101@gmail.com>
2026-08-11 17:34:22 -0700

    Create basic school result portal homepage

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>School Result Portal</h1>
+
+      <p>
+        A simple way to access student results.
+      </p>
+
+      <button>View Result</button>
+    </div>
+  )
+}
+
+export default App
